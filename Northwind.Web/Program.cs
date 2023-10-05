feacd73efabe54af23762f88f9e50cf37d34053e
Max Burrows <maxajburrows@gmail.com>
2023-10-05 18:15:28 +0200

    Launch webpage with ASP.NET Core

---
+++ Northwind.Web/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+WriteLine("This executes after the web server has stopped!");
