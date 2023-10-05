fb28a2a507f84b911ddd60c4388c1b9d4d9cabb3
Max Burrows <maxajburrows@gmail.com>
2023-10-05 17:03:22 +0200

    Create class library for database context

--- Northwind.Common.DataContext.Sqlite/NorthwindContext.cs
+++ Northwind.Common.DataContext.Sqlite/NorthwindContext.cs
@@ -40,19 +40,8 @@ public partial class NorthwindContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Category>(entity =>
-        {
-            entity.Property(e => e.CategoryId).ValueGeneratedNever();
-        });
-
-        modelBuilder.Entity<Employee>(entity =>
-        {
-            entity.Property(e => e.EmployeeId).ValueGeneratedNever();
-        });
-
         modelBuilder.Entity<Order>(entity =>
         {
-            entity.Property(e => e.OrderId).ValueGeneratedNever();
             entity.Property(e => e.Freight).HasDefaultValueSql("0");
         });
 
@@ -62,29 +51,20 @@ public partial class NorthwindContext : DbContext
             entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
 
             entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails).OnDelete(DeleteBehavior.ClientSetNull);
-
             entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails).OnDelete(DeleteBehavior.ClientSetNull);
         });
 
         modelBuilder.Entity<Product>(entity =>
         {
-            entity.Property(e => e.ProductId).ValueGeneratedNever();
             entity.Property(e => e.Discontinued).HasDefaultValueSql("0");
             entity.Property(e => e.ReorderLevel).HasDefaultValueSql("0");
             entity.Property(e => e.UnitPrice).HasDefaultValueSql("0");
             entity.Property(e => e.UnitsInStock).HasDefaultValueSql("0");
             entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("0");
         });
-
-        modelBuilder.Entity<Shipper>(entity =>
-        {
-            entity.Property(e => e.ShipperId).ValueGeneratedNever();
-        });
-
-        modelBuilder.Entity<Supplier>(entity =>
-        {
-            entity.Property(e => e.SupplierId).ValueGeneratedNever();
-        });
+        modelBuilder.Entity<Product>()
+            .Property(product => product.UnitPrice)
+            .HasConversion<double>();
 
         OnModelCreatingPartial(modelBuilder);
     }
